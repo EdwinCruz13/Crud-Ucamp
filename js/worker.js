27246262db3500676d0e9c1465ad0fcfd61752bd
Edwin Cruz <edwin.cruz13@hotmail.com>
2022-11-22 21:52:32 -0600

    adding new changes

--- js/worker.js
+++ js/worker.js
@@ -1,6 +1,27 @@
 var Tasks = [];
 //localStorage.setItem( "Tasks", JSON.stringify(Tasks) );
 
+
+LoadData();
+
+/*var section = document.getElementById("section-card");
+section.innerHTML = CreateCard(1, "task.Title", "task.Description");
+section.innerHTML += CreateCard(2, "task.Title", "task.Description");
+section.innerHTML += CreateCard(3, "task.Title", "task.Description");*/
+
+//Load the tasks saved in the localstorage "Tasks"
+function LoadData()
+{
+    var section;
+    _itemTasks = JSON.parse( localStorage.getItem( "Tasks" ) ); 
+    //fetch the array
+    _itemTasks.map((task) => {
+       section = document.getElementById("section-card");
+       section.innerHTML += CreateCard(1, task.Title, task.Description);
+    });
+
+}
+
 //Save new task
 function SaveTask()
 {
@@ -11,7 +32,11 @@ function SaveTask()
 
     //verify localstarage
     Tasks = JSON.parse( localStorage.getItem( "Tasks" ) );
+    console.log(Tasks);
+
     var filters = Tasks.filter(item => item.Title == title.value);
+
+    
 
     //if there is not match, push a new element into localstarage
     if(filters.length == 0){
@@ -37,5 +62,41 @@ function SaveTask()
     }
     else    
         alert("There is a task using the same Title");
+
+
+    //load the data
+    Tasks = [];
+    LoadData();
         
+}
+
+function CreateCard(_id, _title, _descripcion)
+{
+    var strcad = "";
+    strcad = "<div data-id='" + _id + "' class='card'>"
+    strcad += "<div class='image'>"
+    strcad += "<img"
+    strcad += "    src='./resources/task-inprocess.png'"
+    strcad += "    alt=''"
+    strcad += "  />"
+    strcad += "  <span class='state'>En proceso</span>"
+    strcad += "</div>"
+
+    strcad += "<div class='details'>"
+    strcad += "  <span class='badge'>" + _id + "</span>"
+
+    strcad += "  <h1>" + _title + "</h1>"
+    strcad += "  <p class='information'>"
+    strcad +=  _descripcion
+    strcad += "  </p>"
+
+    strcad += "  <div class='btn-group'>"
+    strcad += "      <button class='btn btn-primary'>Terminar</button>"
+    strcad += "      <button class='btn btn-warning'>Editar</button>"
+    strcad += "      <button class='btn btn-danger'>Anular</button>"
+    strcad += "  </div>"
+    strcad += "</div>"
+    strcad += "</div>" 
+
+    return strcad;
 }
