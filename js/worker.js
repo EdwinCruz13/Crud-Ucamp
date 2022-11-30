dbb10c4abc23d58263f4178c22608255b8c87ed6
edwin.cruz <edwin.cruz@EDWIC>
2022-11-30 13:59:07 -0600

    finalizando proyecto

--- js/worker.js
+++ js/worker.js
@@ -11,14 +11,17 @@ LoadData();
 function LoadData()
 {
     //get Id from section card
+    var countedPending = document.getElementById("counted-task-pending");
     var Card = document.getElementById("section-card");
     Card.innerHTML = '';
 
     //get Id from done task from div
+    var countedCompleted = document.getElementById("counted-task-completed");
     var CompletedDiv = document.getElementById("Done-Task");
     CompletedDiv.innerHTML = '<h2>Tareas completadas</h2>';
 
     //get Id from Cancelled task from div
+    var countedCanceled = document.getElementById("counted-task-canceled");
     var CancelledDiv = document.getElementById("Cancelled-Task");
     CancelledDiv.innerHTML = '<h2>Tareas eliminadas</h2>';
    
@@ -30,7 +33,7 @@ function LoadData()
         
 
             //fetch only the pending task, use filter + map
-            _itemTasks.filter((item) => item.State == 0).map((task) => {
+            var pending = _itemTasks.filter((item) => item.State == 0).map((task) => {
                 if(task.State == 0)
                 _state = "En proceso";
 
@@ -43,16 +46,20 @@ function LoadData()
                 Card.innerHTML += CreateCard(task.id, task.Title, task.Description, _state);
 
             });
+            countedPending.innerHTML = pending.length;
 
             //fetch only the completed task, use filter + map
-            _itemTasks.filter((item) => item.State == 1).map((task) => {
+            var completed =_itemTasks.filter((item) => item.State == 1).map((task) => {
                 CompletedDiv.innerHTML += CreateDoneList(task.id, task.Title, task.Description, _state);
             });
+            countedCompleted.innerHTML = completed.length;
 
             //fetch only the canceled task, use filter + map
-            _itemTasks.filter((item) => item.State == -1).map((task) => {
+            var canceled = _itemTasks.filter((item) => item.State == -1).map((task) => {
                 CancelledDiv.innerHTML += CreateCancelledList(task.id, task.Title, task.Description, _state);
             });
+            countedCanceled.innerHTML = canceled.length;
+
 
     }
 }
@@ -73,48 +80,43 @@ function SaveTask()
         filters = Tasks.filter(item => item.Title == title.value);
     }
 
+    //check out if we are editing or adding new element
+    if(isEdit == false){
+        //if there is not match, push a new element into localstarage
+        if(filters.length == 0){
+             //clear localstorage
+             localStorage.clear();
+             //get the max Id
+             var IdMax = Math.max(...Tasks.map(itemMax => itemMax.id))
+             IdMax = (Tasks.length == 0) ? 0 : IdMax;
+ 
+ 
+             var Task = { id: IdMax + 1, Title: title.value, Description: description.value, DueTo: "", State: 0}
+             Tasks.push(Task);  
 
-    //if there is not match, push a new element into localstarage
-    if(filters.length == 0){
-        
-        //check out if we are editing or adding new element
-        if (isEdit == false){ //adding new element
-
-            //clear localstorage
-            localStorage.clear();
-            //get the max Id
-            var IdMax = Math.max(...Tasks.map(itemMax => itemMax.id))
-            IdMax = (Tasks.length == 0) ? 0 : IdMax;
-
-
-            var Task = { id: IdMax + 1, Title: title.value, Description: description.value, DueTo: "", State: 0}
-            Tasks.push(Task);  
-
+            //add items into localstorage
+            localStorage.setItem( "Tasks", JSON.stringify(Tasks) );
         }
-
-        else{
-            //replacing element because the state changed
-            var Tasks = Tasks.map((elements) => {
-                if(elements.id === parseInt(valueID)){
-                    elements.Title = title.value;
-                    elements.Description = description.value;
-                }
-                return elements
-            });
-        }
-
-        //add items into localstorage
-        localStorage.setItem( "Tasks", JSON.stringify(Tasks) );
-        
-        
-
+        else    
+            alert("There is a task using the same Title");
     }
-    else    
-        alert("There is a task using the same Title");
 
-    
+    else{
+         //replacing element because the state changed
+         var Tasks = Tasks.map((elements) => {
+            if(elements.id === parseInt(valueID)){
+                elements.Title = title.value;
+                elements.Description = description.value;
+                elements.State = 0
+            }
+            return elements
+        });
 
-    
+         //add items into localstorage
+        localStorage.setItem( "Tasks", JSON.stringify(Tasks) );
+    }
+
+
     //load the data
     title.value = "";
     description.value = "";
@@ -229,7 +231,8 @@ function CreateDoneList(_id, _title, _descripcion, _state)
 {
     var strcad = "";
     strcad += "<ul>";
-    strcad += "<li data-id='" + _id + "'>" + _title + "</li>";
+    strcad += "<li data-id='" + _id + "'> <b>"+ _id +".</b>" + _title + "</li>";
+    strcad += "<small class='tag-description'>" + _descripcion + "</small>"
     strcad += "</ul>";
     return strcad;
 }
@@ -239,12 +242,13 @@ function CreateCancelledList(_id, _title, _descripcion, _state)
     var strcad = "";
     strcad += "<ul>";
 
-    strcad += "<li>";
+    strcad += "<li> <b>"+ _id +".</b>";
     strcad += "<span>" + _title + "</span>";
     strcad += "<div class='btn-group'>";
-    strcad += "<button class='btn btn-warning' data-id='" + _id + "'>Editar</button>";
+    strcad += "<button class='btn btn-warning' data-id='" + _id + "' onclick='EditTask(this)'>Editar</button>";
     strcad += "</div>";
     strcad += "</li>";
+    strcad += "<small class='tag-description'>" + _descripcion+ "</small>"
     strcad += "</ul>";
     
     return strcad;
